3632d1a456287adb3c0d7e589a43d3c99c3bcaf1
Patrick Gibson <patrickgibson@Patricks-MacBook-Air.local>
2016-09-09 18:09:05 -0600

    friday

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
+var app = angular.module('dragulaTest', [angularDragula(angular)]);
 
+
+app.controller('MainCtrl', function($scope, dragulaService) {
+  dragulaService.options($scope, 'first-bag', {
+      copy: true
       
+    });
+});
+
+
+
+//add image prompt
+function imagePrompt (){
+var img = new Image();
+    img.src = prompt("Url of a picture:");
+    
+    img.onerror = function() { alert("Provided URL does not point to a valid picture.") };
+    
+    
+    img.onload = function() {
+        document.getElementById("yourImage").src = img.src;
+    };
+}
